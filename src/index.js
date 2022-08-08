5a68e78daed6f1095594cd7336644aa871b983c6
vadim-sid1993 <79473969+vadim-sid1993@users.noreply.github.com>
2022-08-08 12:38:42 +0300

    init: task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        arr.push(bracketsConfig[i].join(''))
+    };
+    for (let i = 0; i < arr.length; i++) {
+        if (str.includes(arr[i])) {
+            str = str.replace(arr[i], '');
+            i = -1;
+        }
+    };
+    if (str) {
+        return false
+    } else {
+        return true
+    }
 }
